a040b16959eb92c81b838914dfab570cdcc30014
krocarondinel <rocarondinel.kelly@gmail.com>
2020-03-24 23:13:41 -0500

    add changes in Portfolio

---
+++ js/contact_me.js
@@ -0,0 +1,39 @@
+const clickCv = document.querySelector('#btn-cv')
+clickCv.addEventListener('click', () => window.open('pdf/CV-KellyRocaRondinel.pdf')
+);
+
+document.querySelector('#repo1').addEventListener('click', () =>
+window.open('https://github.com/KellyRoca/LIM011-data-lovers'));
+
+document.querySelector('#demo1').addEventListener('click', () =>
+window.open('https://kellyroca.github.io/LIM011-data-lovers/src/'));
+
+
+//
+document.querySelector('#repo2').addEventListener('click', () =>
+window.open('https://github.com/KellyRoca/LIM011-fe-social-network'));
+
+document.querySelector('#demo2').addEventListener('click', () =>
+window.open('https://kellyroca.github.io/LIM011-fe-social-network/SPA/'));
+
+//
+document.querySelector('#repo3').addEventListener('click', () =>
+window.open('https://github.com/KellyRoca/LIM011-fe-md-links'));
+
+// document.querySelector('#demo3').addEventListener('click', () =>
+// window.open('https://kellyroca.github.io/LIM011-data-lovers/src/'));
+
+// buttons of contact
+document.querySelector('#logoWhatsapp').addEventListener('click', () => 
+window.open('https://api.whatsapp.com/send?phone=51953832070&text='));
+
+document.querySelector('#logoGithub').addEventListener('click', () => 
+window.open('https://github.com/KellyRoca'));
+
+document.querySelector('#logoLinkedin').addEventListener('click', () => 
+window.open('https://www.linkedin.com/in/kelly-roca-rondinel/'));
+
+document.querySelector('#logoGmail').addEventListener('click', () => 
+window.open('https://mail.google.com/mail/u/0/?view=cm&fs=1&to=rocarondinel.kelly@gmail.com&tf=1'));
+
+
